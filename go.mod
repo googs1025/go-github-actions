292498deb670edd608a6e97407a8096f2a4c92fe
googs1025 <googs1025@gmail.com>
2022-09-13 16:44:32 +0800

    push

---
+++ go.mod
@@ -0,0 +1,3 @@
+module golanglearning/new_project/go-github-actions
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"golanglearning/new_project/go-github-actions/hello"
+)
+
+func main() {
+	fmt.Println(hello.Greet())
+}
